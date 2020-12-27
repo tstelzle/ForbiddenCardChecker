6dcb8775f872ac7c786c522855f2a350dd309c9e
Tarek <tarek.stelzle@gmx.de>
2020-12-27 12:56:19 +0100

    Implemented API

---
+++ api.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask_restful import Api, Resource
+
+from modules.module_check import check_forbidden_cards
+from modules.module_read_data import get_forbidden_cards
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class ForbiddenCard(Resource):
+
+    def get(self, names: str):
+        link_german = "https://img.yugioh-card.com/de/gameplay/detail.php?id=1166"
+        link_english = "https://img.yugioh-card.com/oc/gameplay/detail.php?id=1155"
+
+        forbidden_cards_german = get_forbidden_cards(link_german, False)
+        forbidden_cards_english = get_forbidden_cards(link_english, False)
+
+        deck_cards = [card.lower() for card in names.split(';')]
+
+        forbidden_deck_german = check_forbidden_cards(forbidden_cards_german, deck_cards)
+        forbidden_deck_english = check_forbidden_cards(forbidden_cards_english, deck_cards)
+
+        return [forbidden_deck_german[0] + forbidden_deck_english[0],
+                forbidden_deck_german[1] + forbidden_deck_english[1]], 200
+
+
+api.add_resource(ForbiddenCard, "/forbiddenCard/<string:names>")
+
+if __name__ == "__main__":
+    app.run(debug=True, host='localhost', port=9999)
